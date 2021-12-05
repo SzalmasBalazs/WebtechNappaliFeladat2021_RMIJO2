24315a9609ea7c5cd5fc133dbdc1c39ae3f2e10c
SzalmasBalazs <78908375+SzalmasBalazs@users.noreply.github.com>
2021-12-05 17:00:51 +0100

    beadando haladas

--- projectscripts.js
+++ projectscripts.js
@@ -1,15 +1,32 @@
 function emailValidation(inputText) {
 
     var mailFormat =/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-
+    var booleanValue;
     if (inputText.value.match(mailFormat)) {
         document.form1.email.focus();
-        return true;
+        booleanValue =formConfirmBox(booleanValue);
+        return booleanValue;
     } else {
         alert("Invalid E-mail adress!");
         document.form1.email.focus();
-        return false;
+        booleanValue = false
+        return booleanValue;
     }
-    
-    
+}
+
+function formConfirmBox(booleanValue){
+ 
+    if(confirm("Are you sure you want to add these details?")){
+        booleanValue = true;
+        alert("Details added.");
+        return booleanValue;
+    } else {
+        booleanValue = false;
+       alert("Canceled!");
+       return booleanValue;
+    }
+
+}
+function hideForm(){
+    document.getElementById("form").style.display =" hide";
 }
